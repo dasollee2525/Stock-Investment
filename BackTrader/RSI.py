06d2d8a0d8899d0685867b7d46e87801267d5475
dasollee2525 <90080808+dasollee2525@users.noreply.github.com>
2022-07-27 21:36:51 +0900

    Update RSI.py

--- BackTrader/RSI.py
+++ BackTrader/RSI.py
@@ -1,6 +1,12 @@
 from datetime import datetime
 import backtrader as bt
 import yfinance as yf
+from matplotlib import warnings
+from matplotlib.dates import (HOURS_PER_DAY, MIN_PER_HOUR, SEC_PER_MIN,
+    MONTHS_PER_YEAR, DAYS_PER_WEEK, SEC_PER_HOUR,
+    SEC_PER_DAY, num2date, rrulewrapper,
+    YearLocator, MicrosecondLocator)
+
 
 class MyStrategy(bt.Strategy):  
     def __init__(self):
@@ -15,7 +21,7 @@ class MyStrategy(bt.Strategy):
 
 cerebro = bt.Cerebro()  
 cerebro.addstrategy(MyStrategy)
-data = bt.feeds.PandasData(dataname=yf.download('036570.KS', '2021-06-01', '2021-10-09', auto_adjust=True))
+data = bt.feeds.PandasData(dataname=yf.download('036570.KS', '2017-06-01', '2021-10-09', auto_adjust=True))
 cerebro.adddata(data)
 cerebro.broker.setcash(10000000) 
 cerebro.addsizer(bt.sizers.SizerFix, stake=30)  
